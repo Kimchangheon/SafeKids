12ac42d46449a34a0f5d141480ff7a280c02d39d
xayahfirst <diablo8273@gmail.com>
2018-07-30 16:12:02 +0900

    ui update

--- app/src/main/java/com/example/changheonkim/safekids/MainActivity.java
+++ app/src/main/java/com/example/changheonkim/safekids/MainActivity.java
@@ -14,11 +14,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button login = (Button)findViewById(R.id.login);
+        Button tempAdmin = (Button)findViewById(R.id.tempAdmin);
+        Button tempParents = (Button)findViewById(R.id.tempParents);
+        Button tempChild = (Button)findViewById(R.id.tempChild);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mainActivity = new Intent(getApplicationContext(),Admin.class);
+                startActivityForResult(mainActivity,REQUEST_CODE_LOGIN);
+            }
+        });
+
+        tempAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainActivity = new Intent(getApplicationContext(),Admin.class);
+                startActivityForResult(mainActivity,REQUEST_CODE_LOGIN);
+            }
+        });
+
+        tempChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainActivity = new Intent(getApplicationContext(),Child.class);
+                startActivityForResult(mainActivity,REQUEST_CODE_LOGIN);
+            }
+        });
+
+        tempParents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainActivity = new Intent(getApplicationContext(),Parents.class);
                 startActivityForResult(mainActivity,REQUEST_CODE_LOGIN);
             }
         });
