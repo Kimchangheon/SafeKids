3959a0b737bc5a52e3b69aa2ce2165b61b93a083
xayahfirst <diablo8273@gmail.com>
2018-07-31 14:16:28 +0900

    update signup

--- app/src/main/java/com/example/changheonkim/safekids/SignUp.java
+++ app/src/main/java/com/example/changheonkim/safekids/SignUp.java
@@ -2,6 +2,8 @@ package com.example.changheonkim.safekids;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //signup spinner
+        Spinner signUpSpinner = (Spinner)findViewById(R.id.signUp);
+        ArrayAdapter signUpAdapter = ArrayAdapter.createFromResource(this,R.array.signUp,android.R.layout.simple_spinner_item);
+        signUpAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signUpSpinner.setAdapter(signUpAdapter);
     }
 }
