2a38e64b4e566750cc04bcae235d550f6eedfb12
xayahfirst <diablo8273@gmail.com>
2018-07-31 15:21:16 +0900

    Spinner update

--- app/src/main/java/com/example/changheonkim/safekids/Admin.java
+++ app/src/main/java/com/example/changheonkim/safekids/Admin.java
@@ -1,13 +1,62 @@
 package com.example.changheonkim.safekids;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Admin extends AppCompatActivity {
+
+    //value
+    int StudentNum = 1;
+
+
+    //widget
+    TableRow admin_tableRow;
+    Spinner adminSpinner;
+    TextView studentName;
+    TextView studentNumber;
+    TextView studentNameInput;
+    LinearLayout linearLayout;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        admin_tableRow = (TableRow)findViewById(R.id.admin_tableRow);
+        adminSpinner = (Spinner)findViewById(R.id.adminSpinner);
+        studentName = (TextView)findViewById(R.id.studentName);
+        studentNumber = (TextView)findViewById(R.id.studentNumber);
+        studentNameInput = (TextView)findViewById(R.id.studentNameInput);
+        studentNameInput = (TextView)findViewById(R.id.studentNameInput);
+        linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
+        context = this;
+
+        String StudentNum_string = Integer.toString(StudentNum);
+
+        studentNumber.setText(StudentNum_string);
+
+        //signup spinner
+        adminSpinner.setSelection(0);
+        ArrayAdapter adminAdapter = ArrayAdapter.createFromResource(this,R.array.admin,android.R.layout.simple_spinner_item);
+        adminAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adminSpinner.setAdapter(adminAdapter);
+
+        makeAdminRow();
+
+    }
+
+    //StudentNum의 값에 따른 SignupRow를 동적생성
+    public void makeAdminRow() {
+        for (int i = 0; i < StudentNum; i++) {
+            TableRow admintablerow = new TableRow(context);
+
+        }
     }
 }
--- app/src/main/java/com/example/changheonkim/safekids/SignUp.java
+++ app/src/main/java/com/example/changheonkim/safekids/SignUp.java
@@ -12,35 +12,12 @@ import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
-    //widget
-    int Studentnum = 0;
-    TableRow signUp_tableRow= (TableRow)findViewById(R.id.signUp_tableRow);;
-    Spinner signUpSpinner= (Spinner)findViewById(R.id.signUpSpinner);
-    TextView studentName = (TextView)findViewById(R.id.studentName);
-    TextView studentNameInput = (TextView)findViewById(R.id.studentNameInput);
-    LinearLayout linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
-    Context context = this;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-        //signup spinner
-        signUpSpinner.setSelection(0);
-        ArrayAdapter signUpAdapter = ArrayAdapter.createFromResource(this,R.array.signUp,android.R.layout.simple_spinner_item);
-        signUpAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        signUpSpinner.setAdapter(signUpAdapter);
-
-        makesignUpRow();
-
-    }
-    //StudentNum의 값에 따른 SignupRow를 동적생성
-    public void makesignUpRow() {
-        for (int i = 0; i < Studentnum; i++) {
-            TableRow signUptablerow = new TableRow(context);
-
-        }
     }
 }
