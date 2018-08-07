5f977e0df8eed0bd63e8145a2160d200924e9586
xayahfirst <diablo8273@gmail.com>
2018-08-07 17:19:23 +0900

    signup_divide activity add

--- app/src/main/java/com/example/changheonkim/safekids/Board.java
+++ app/src/main/java/com/example/changheonkim/safekids/Board.java
@@ -3,7 +3,7 @@ package com.example.changheonkim.safekids;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class board extends AppCompatActivity {
+public class Board extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
